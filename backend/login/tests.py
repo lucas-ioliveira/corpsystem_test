b1a77fa72f16b81c0b2d22449998e632fe9b749b
Lucas Oliveira <lucasio2008@gmail.com>
2024-02-10 20:35:33 -0300

    feat(corpsystem/login): Teste unitário.
    Desenvolvimento de teste unitário para o app login.

--- backend/login/tests.py
+++ backend/login/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LoginTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='admin',
+            password='admin123',
+        )
+
+    def test_login(self):
+        assert self.client.login(username='admin', password='admin123')
